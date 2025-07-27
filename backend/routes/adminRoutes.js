cd30698c8d7e1aec84ade23e8c3ba8af24eecade
Lokesh kumar <lokesh121itc21@bpitindia.edu.in>
2025-07-27 19:03:29 +0530

    Delete backend directory

--- backend/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const adminController = require('../controllers/adminController');
-
-const router = express.Router();
-
-router.post('/send-message', adminController.sendMessageToAllWorkers);
-
-module.exports = router;
